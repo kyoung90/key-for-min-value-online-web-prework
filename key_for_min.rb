8e7d698c6d39e755206f9f19b53fa1679a41b0f1
Kenneth Young Castro <kenneth.young90@gmail.com>
2018-10-14 17:13:23 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,12 @@
 
 def key_for_min_value(name_hash)
 
+ min_value = name_hash.shift[0]
+ name_hash.each do |key, value|
+   if (name_hash[min_value] > value)
+     min_value = key
+   end 
+ end 
+ 
+ return min_value
 end
